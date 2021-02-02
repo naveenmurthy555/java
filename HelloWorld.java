37612563e1110d6b0b13617360b0f22070afa87a
naveenmurthy555 <naveenmurthy555@gmail.com>
2021-02-02 12:37:01 +0530

    chk22

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello All, Naveen and Rakshit");
+      System.out.println("hi All, Naveen and Rakshit");
    }
 }
